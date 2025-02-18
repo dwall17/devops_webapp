288986cc4c21050787b0a35c0ce47d8e992aa03c
Dylan Wallace <dylanwallace17@gmail.com>
2025-02-17 22:43:49 -0500

    add a sg that which allows ssh

--- terraform/ec2/jenkins.tf
+++ terraform/ec2/jenkins.tf
@@ -3,10 +3,44 @@ variable "jenkins_name" {
   default     = "jenkins"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins-sg"
+  description = "Allow SSH inbound traffic"
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic (this is common for security groups)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "jenkins-sg"
+  }
+}
+
 resource "aws_instance" "devops_01_jenkins" {
   ami           = "ami-053a45fff0a704a47"
   instance_type = "t2.micro"
   key_name      = "my-first-keypair"
+
+  # Attach the security group created above
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
 
   tags = {
     Name = "${var.jenkins_name}"
